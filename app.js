91b334a61a1dee7707b618d9c05658f58e006dc1
Vanessa <vanedasilva100@gmail.com>
2023-11-06 22:07:16 -0300

    add function salvar

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+function salvar(){
+    let idcidade = document.getElementById('idcidade').value
+    let cidade = document.getElementById('cidade').value 
+    let estado = document.getElementById('estado').value
+
+    let url
+
+    if(idcidade == ''){
+        url = `./back/cidade_inserir.php?idcidade=${idcidade}&cidade=${cidade}&estado=${estado}`
+    }
+    else {
+        url = `./back/cidade_alterar.php?idcidade=${idcidade}&cidade=${cidade}&estado=${estado}`
+    }
+
+    let req = new XMLHttpRequest()
+    req.open('GET', url, false)
+    req.send()
+    let res = JSON.parse(req.responseText)
+    console.log(res)
+}
+
 function selecionar(idcidade) {
     let url = `./back/cidade_selecionar.php?idcidade=${idcidade})`
     let req = new XMLHttpRequest()
@@ -13,27 +34,27 @@ function selecionar(idcidade) {
 
 function listar(){
     //Monta a url que será executada para chamar o back-end
-    let url = `./back/cidade_listar.php`;
+    let url = `./back/cidade_listar.php`
     
     //Cria a requisição http
-    let req = new XMLHttpRequest();
+    let req = new XMLHttpRequest()
     
     //Configura a requisição
-    req.open('GET', url, false);
+    req.open('GET', url, false)
     
     //Executa a requisição
-    req.send();
+    req.send()
     
     //Transforma a resposta recebida do back-end em formato json
-    let res = JSON.parse(req.responseText);
+    let res = JSON.parse(req.responseText)
     
     //Exibe a resposta no console
-    console.log(res);
+    console.log(res)
     //Capturar o elemento TBody da table
-    let t = document.getElementById(`tabela-cidade`);
+    let t = document.getElementById(`tabela-cidade`)
     
     //Limpar as linhas existentes
-    t.innerHTML = '';
+    t.innerHTML = ''
     //Fazer um loop na lista de cidade e desenhar o html na table
     for (i in res){
     t.innerHTML = t.innerHTML + `<tr>
@@ -46,6 +67,6 @@ function listar(){
     </td>
     <td><button onclick="excluir(${res[i].idcidade})"
     class="btn btn-danger">Excluir</button></td>
-    </tr>`;
+    </tr>`
     }
 }
